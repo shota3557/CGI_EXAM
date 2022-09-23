4469fb946ec8d46f69c18e859855f90c23aec827
shota kikuchi <shota3557@gmail.com>
2022-09-23 12:56:18 +0900

    課題提出のため

---
+++ test.rb
@@ -0,0 +1,19 @@
+require 'webrick'
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+
+server.mount('/quality.cgi', WEBrick::HTTPServlet::CGIHandler, 'quality.rb')
+
+# この一行を追記
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+server.start
